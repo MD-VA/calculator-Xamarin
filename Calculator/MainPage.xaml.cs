7c176e5800f663d6a55ec5a332c80512aa2cf7d5
Anouch Mouad <mdanouch@Anouchs-MacBook-Pro.local>
2022-01-04 23:25:42 +0100

    first commit

---
+++ Calculator/MainPage.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Calculator
+{
+
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        
+        private decimal firstNumber;
+        private string operatorName; 
+        private bool isClickedOperator = false;
+
+
+        private void OperationButton(System.Object sender, System.EventArgs e)
+        {
+            var button = sender as Button;
+            isClickedOperator = true;
+            operatorName = button.Text;
+            firstNumber = Convert.ToDecimal(result.Text);
+
+        }
+
+        private void btnEqual_clicked(System.Object sender, System.EventArgs e)
+        {
+            try
+            {
+                decimal secondNumber = Convert.ToDecimal(result.Text);
+                string fnResult = caluclate(firstNumber, secondNumber, operatorName).ToString("0.##");
+                result.Text = fnResult;
+            }
+            catch(Exception ex)
+            {
+                DisplayAlert("Error", ex.Message, "OK");
+            }
+        }
+
+
+        private decimal caluclate(decimal firstNumber, decimal secondNumber, string operatorName)
+        {
+            decimal fnResult = 0;
+            switch (operatorName)
+            {
+                case "+":
+                    fnResult = firstNumber + secondNumber;
+                    break;
+                case "-":
+                    fnResult = firstNumber - secondNumber;
+                    break;
+                case "X":
+                    fnResult = firstNumber * secondNumber;
+                    break;
+                case "/":
+                    fnResult = firstNumber / secondNumber;
+                    break;
+            }
+
+            return fnResult;
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var button = sender as Button;
+            if (result.Text =="0" || isClickedOperator)
+            {
+                isClickedOperator = false;
+                result.Text = button.Text;
+            }
+            else
+            {
+                result.Text += button.Text;
+            }
+        }
+
+        void clearBtn_Clicked(System.Object sender, System.EventArgs e)
+        {
+            result.Text = "0";
+            isClickedOperator = false;
+            firstNumber = 0;
+
+        }
+
+        void suppBtn_Clicked(System.Object sender, System.EventArgs e)
+        {
+            string number = result.Text;
+            if(number != "0")
+            {
+                number = number.Remove(number.Length - 1, 1);
+                Console.WriteLine(number);
+                if (string.IsNullOrEmpty(number))
+                {
+                    result.Text = "0";
+                }else
+                {
+                    result.Text = number;
+                }
+            }
+        }
+
+        private async void pourcentageBtn_Clicked(System.Object sender, System.EventArgs e)
+        {
+            try
+            {
+                string number = result.Text;
+                if (number!= "0") {
+                    decimal percentValue = Convert.ToDecimal(number);
+                    string value = (percentValue / 100).ToString("0.##");
+                    result.Text = value;
+                }
+            }catch(Exception ex)
+            {
+                await DisplayAlert("Error",ex.Message,"OK");
+            }
+        }
+
+    }
+}
